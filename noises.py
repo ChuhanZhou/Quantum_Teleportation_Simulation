30b1c90723bb7e6e7569124041a573f6d1ab32d8
ChuhanZhou <60944082+ChuhanZhou@users.noreply.github.com>
2024-08-22 14:53:02 +0200

    add disambiguate comments

--- noises.py
+++ noises.py
@@ -17,7 +17,9 @@ def dephasing_noise(density_matrix, gamma=0.0, z=0.0):
     for i in range(qubit_num):
         plan.append(["I", [i]])
     I = circuits.Circuit(qubit_num, plan).circuit_matrix
-    # ρ' = (1 - γ) * ρ + γ * I * (z*ρ+(1-z)*I)
+    # All gates here are working as a matrix, a object, not calculated as a gate in circuit
+    # [I] is a selector
+    # ρ' = (1 - γ) * ρ + γ * [I] * (z*ρ+(1-z)*I)
     # ρ'z => zρ+(1-z)*I
     #   z = 0, ρ'z => I
     #   z = 1, ρ'z => 0
